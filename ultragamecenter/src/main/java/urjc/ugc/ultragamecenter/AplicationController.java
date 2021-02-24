94ed59a9ef259e28766f57df636276f135cf685e
jesus <jesuselez99@gmail.com>
2021-02-24 17:20:26 +0100

    web pages created

--- ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/AplicationController.java
+++ ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/AplicationController.java
@@ -5,9 +5,47 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AplicationController{
-    @GetMapping("/Ejemplo")
-    public String ejemploAnuncio(Model model){
-        model.addAttribute("nombre","Mundo!");
-        return "EjemploTemplate";
+    @GetMapping("/")
+    public String getIndex(Model model){
+        model.addAttribute("nombre","Index");
+        return "IndexTemplate";
     }
+
+    @GetMapping("/Admin")
+    public String getAdmin(Model model){
+        model.addAttribute("nombre","Admin");
+        return "AdminTemplate";
+    }
+
+    @GetMapping("/User")
+    public String getUser(Model model){
+        model.addAttribute("nombre","User Page");
+        return "UserTemplate";
+    }
+
+    @GetMapping("/Register")
+    public String getLoginRegister(Model model){
+        model.addAttribute("nombre","User Page");
+        return "LoginRegisterTemplate";
+    }
+
+    @GetMapping("/Reservation")
+    public String getReservation(Model model){
+        model.addAttribute("nombre","User Page");
+        return "ReservationTemplate";
+    }
+
+    @GetMapping("/SingleEvent")
+    public String getSingleEvent(Model model){
+        model.addAttribute("nombre","User Page");
+        return "SingleEventTemplate";
+    }
+
+    @GetMapping("/Events")
+    public String getEvents(Model model){
+        model.addAttribute("nombre","User Page");
+        return "EventsTemplate";
+    }
+
+
 }
