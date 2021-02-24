ab0178d6399471785250d1b721a4a3b277340926
santo2927 <santo2927@gmail.com>
2021-02-24 17:25:46 +0100

    Moving templates

--- ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/AplicationController.java
+++ ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/AplicationController.java
@@ -13,37 +13,37 @@ public class AplicationController{
 
     @GetMapping("/Admin")
     public String getAdmin(Model model){
-        model.addAttribute("nombre","Admin");
+        model.addAttribute("nombre2","Admin");
         return "AdminTemplate";
     }
 
     @GetMapping("/User")
     public String getUser(Model model){
-        model.addAttribute("nombre","User Page");
+        model.addAttribute("nombre3","User Page");
         return "UserTemplate";
     }
 
     @GetMapping("/Register")
     public String getLoginRegister(Model model){
-        model.addAttribute("nombre","User Page");
+        model.addAttribute("nombre4","Register Page");
         return "LoginRegisterTemplate";
     }
 
     @GetMapping("/Reservation")
     public String getReservation(Model model){
-        model.addAttribute("nombre","User Page");
+        model.addAttribute("nombre5","Reservation Page");
         return "ReservationTemplate";
     }
 
     @GetMapping("/SingleEvent")
     public String getSingleEvent(Model model){
-        model.addAttribute("nombre","User Page");
+        model.addAttribute("nombre6","SingleEvent Page");
         return "SingleEventTemplate";
     }
 
     @GetMapping("/Events")
     public String getEvents(Model model){
-        model.addAttribute("nombre","User Page");
+        model.addAttribute("nombre","Events Page");
         return "EventsTemplate";
     }
 
