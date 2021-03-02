4d224a80ed98fc40f6ffe9ac5e0a8528fc04f027
Miguel <micenor88@gmail.com>
2021-03-02 18:07:46 +0100

    getter setted table entity

--- ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/Table.java
+++ ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/Table.java
@@ -1,9 +1,42 @@
 package urjc.ugc.ultragamecenter;
 
+import javax.persistence.Entity;
 
+@Entity
 public class Table {
     private Long id;
     private String type;
     private final String possibleTypes[]={"PS4","PC","XBOX"};
     private Boolean free;
+
+
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public Boolean getFree(){
+        return this.free;
+    }
+
+    public void setFree(Boolean free){
+        this.free = free;
+    }
+
 }
+
+
+
+
