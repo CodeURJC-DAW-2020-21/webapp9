850dbccc577938a62a660bb5abdbe43bc40f7f89
Rod <roddesiqueira@gmail.com>
2021-03-22 00:44:40 +0100

    Controller and html fixes

--- ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/Controllers/UserController.java
+++ ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/Controllers/UserController.java
@@ -5,7 +5,6 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -16,11 +15,10 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,7 +43,7 @@ public class UserController {
 	private UserService uservice;
 	
 	@Autowired
-	UserComponent loggedUser;
+	UserComponent userComponent;
 
 	@Autowired
 	EventRepository eRepository;
@@ -62,46 +60,26 @@ public class UserController {
 	private Event editedEvent = null;
 
 	private static final Path IMAGES_FOLDER = Paths.get(System.getProperty("user.dir"), "images");
-
+	
+	
 	public void setHeader(Model model) {
-		model.addAttribute("Logout", this.loggedUser.isLoggedUser() ? "Cerrar sesión" : "");
-		model.addAttribute("Logout-ico", this.loggedUser.isLoggedUser() ? "fa fa-sign-out" : "");
-		model.addAttribute("Admin", this.loggedUser.isAdmin() ? "Administrador" : "");
-		model.addAttribute("Admin-ico", this.loggedUser.isAdmin() ? "fa fa-star" : "");
 
+	}
+	
+	@GetMapping("/")
+	public String getIndex(Model model) {
+		setHeader(model);
+		model.addAttribute("site", "INICIO");
+		return "index";
 	}
 	
 	@PostMapping("/register")
 	public String registerUser(@RequestParam String name,@RequestParam String lastName,@RequestParam String password,@RequestParam String email) {
 		User user = uservice.createNewUser(name,lastName,password,email);
-		loggedUser.setLoggedUser(user);
+		userComponent.setLoggedUser(user);
 		return "redirect:/" ;
 	}
-/*
-	@GetMapping("/")
-	public String getIndex(Model model) {
-		setHeader(model);
-		model.addAttribute("site", "INICIO");
-		return "IndexTemplate";
-	}
-
-
-	@GetMapping("/admin")
-	public String getAdmin(Model model) {
-		model.addAttribute("site", "ADMIN");
-		setHeader(model);
-		if (this.loggedUser.isAdmin()) {
-			model.addAttribute("nombre", "Admin");
-			model.addAttribute("events", eRepository.findAll());
-			model.addAttribute("reservations", trrepository.findAll());
-
-			return "AdminTemplate";
-		} else {
-			return getProfile(model);
-		}
-
-	}
-*/
+	
 	@GetMapping("/reservation")
 	public String getReservation(Model model) {
 		model.addAttribute("site", "MESAS");
@@ -109,7 +87,7 @@ public class UserController {
 		return "ReservationTemplate";
 	}
 
-	@GetMapping("/singleevent")
+	@GetMapping("/single-event")
 	public String getSingleEvent(Model model) {
 		model.addAttribute("site", "EVENTO");
 		setHeader(model);
@@ -121,40 +99,9 @@ public class UserController {
 	public String getEvents(Model model) {
 		setHeader(model);
 		model.addAttribute("site", "EVENTOS");
-		model.addAttribute("events", this.loggedUser.sort(eRepository.findAll()));
-		return "EventsTemplate";
-	}
-	//
-
-	@GetMapping("/admin/event-edit")
-	public String editEvent(@RequestParam String id, Model model) {
-		setHeader(model);
-		Event event = eRepository.findByid(Long.parseLong(id));
-		model.addAttribute("name", event.getName());
-		model.addAttribute("description", event.getDescription());
-		String label = "";
-		for (String x : event.getLavels()) {
-			label += x;
-			label += "/";
-		}
-		model.addAttribute("labels", label);
-		model.addAttribute("capacity", event.getCapacity());
-		model.addAttribute("date", event.getDate().toString());
-		model.addAttribute("site", "EV/ED");
-		this.editedEvent = event;
-		return "EventCreatorTemplate";
-	}
-
-	@GetMapping("/admin/graph-event")
-	public String graphEvent(@RequestParam String id, Model model) {
-		model.addAttribute("site", "GRAFICO");
-		setHeader(model);
-		Event event = eRepository.findByid(Long.parseLong(id));
-		Integer likes = event.getlikes();
-		model.addAttribute("likes", likes);
-		Integer plazasLibres = event.getCapacity() - likes;
-		model.addAttribute("plazasLibres", plazasLibres);
-		return "GraphsEventsTemplate";
+		model.addAttribute("events", this.userComponent.sort(eRepository.findAll()));
+		model.addAttribute("isLogged", this.userComponent.isLoggedUser());
+		return "events";
 	}
 
 	@GetMapping("/events/see-event")
@@ -171,97 +118,19 @@ public class UserController {
 		return getSingleEvent(model);
 	}
 
-	/*
+	
 	@GetMapping("/like")
-	public String likeEvent(@RequestParam String id, Model model) {
+	
+	public String likeEvent(@RequestParam(value="Asistir") String id, Model model) {
 
 		Event event = eRepository.findByid(Long.parseLong(id));
-		if (this.loggedUser.isLoggedUser() && !this.loggedUser.hasLiked(event.getId())) {
-			this.loggedUser.like(event);
-			eRepository.save(event);
-		} else {
-			return getProfile(model);
-		}
-
-		return getEvents(model);
+		//this.userComponent.like(event);
+		event.like();
+		eRepository.save(event);
+		return "/events";
 	}
-	*/
-
-	@GetMapping("/admin/graph-tables")
-	public String graphTables(Model model) {
-		model.addAttribute("Admin", "Administrador");
-		model.addAttribute("Logout", "Cerrar sesión");
-		model.addAttribute("nombre", "Admin");
-		List<TableReservation> reservations = trrepository.findAll();
-
-		Integer numPC = 0;
-		Integer numXBOX_ONE = 0;
-		Integer numPS5 = 0;
-
-		for (TableReservation tableReservation : reservations) {
-			Tablegame table = trepository.findById(tableReservation.getId_table());
-
-			switch (table.getType()) {
-			case "PC":
-				numPC++;
-				break;
-			case "XBOX_ONE":
-				numXBOX_ONE++;
-				break;
-			case "PS5":
-				numPS5++;
-				break;
-			default:
-			}
-		}
-		model.addAttribute("numPC", numPC);
-		model.addAttribute("numXBOX_ONE", numXBOX_ONE);
-		model.addAttribute("numPS5", numPS5);
-		return "GrapsTablesTemplate";
-	}
-
-	/*
-	@GetMapping("/admin/delete-reservation")
-	public String borrarReserva(@RequestParam String id, Model model) {
-		TableReservation reserva = trrepository.findByid(Long.parseLong(id));
-		trrepository.delete(reserva);
-
-		model.addAttribute("events", eRepository.findAll());
-		model.addAttribute("reservations", trrepository.findAll());
-		return getAdmin(model);
-	}
-
-	@GetMapping("/admin/delete-event")
-	public String borrarEvento(@RequestParam String id, Model model) {
-		Event evento = eRepository.findByid(Long.parseLong(id));
-		if (evento != null) {
-			eRepository.delete(evento);
-		}
-		model.addAttribute("events", eRepository.findAll());
-		model.addAttribute("reservations", trrepository.findAll());
-		return getAdmin(model);
-	}
-	*/
-	/*
-
-	@GetMapping("/Event-Adder")
-	public String getEventAdder(Model model) {
-		setHeader(model);
-		if (this.loggedUser.isAdmin()) {
-			model.addAttribute("name", "Nombre del evento*");
-			model.addAttribute("description", "Descripción del evento");
-			model.addAttribute("labels", "SHOOTER/MOBA/MMO/");
-			model.addAttribute("capacity", "");
-			model.addAttribute("date", "0000-00-00");
-			model.addAttribute("site", "EVENTO+");
-			return "EventCreatorTemplate";
-		}
-		return getProfile(model);
-	}
-	*/
 	
 	/*
-
 	@GetMapping("/user")
 	public String getUser(Model model) {
 		if (this.loggedUser.isLoggedUser()) {
@@ -283,19 +152,31 @@ public class UserController {
 	}
 	
 	*/
-	
-	/*
 
 	@GetMapping("/profile")
 	public String getProfile(Model model) {
-		return this.loggedUser.isLoggedUser() ? getUser(model) : getLogin(model);
+		
+		//setHeader(model);
+		boolean isLogged = userComponent.isLoggedUser();
+		User userLogged = userComponent.getLoggedUser();
+		
+		if(!isLogged){
+			return "redirect:/login";
+		}
+		
+		model.addAttribute("admin", userLogged.getRoles().contains("ADMIN"));
+		model.addAttribute("isLogged", isLogged);
+		model.addAttribute("name", userLogged.getName());
+		model.addAttribute("lastname", userLogged.getLastName());
+		model.addAttribute("email", userLogged.getEmail());
+		
+		return "user";
 	}
 	
-	*/
-
+	
 	@GetMapping("/get-user-image")
 	public ResponseEntity<Object> downloadImage(Model model) throws MalformedURLException {
-		User aux = this.loggedUser.getLoggedUser();
+		User aux = this.userComponent.getLoggedUser();
 		Path imagePath = IMAGES_FOLDER.resolve("image" + aux.getEmail() + ".jpg");
 
 		Resource image = new UrlResource(imagePath.toUri());
@@ -308,63 +189,18 @@ public class UserController {
 		model.addAttribute("site", "EDITAR PERFIL");
 		setHeader(model);
 
-		String name = loggedUser.getLoggedUser().getName();
-		String surname = loggedUser.getLoggedUser().getLastName();
+		String name = userComponent.getLoggedUser().getName();
+		String surname = userComponent.getLoggedUser().getLastName();
 		model.addAttribute("Name", name.equals("") ? "Nombre*" : name);
 		model.addAttribute("Surname", name.equals("") ? "Apellidos*" : surname);
 
 		return "EditProfileTemplate";
 	}
-	/*
-	@GetMapping("/register")
-	public String getRegister(Model model) {
-		model.addAttribute("site", "INICIAR SESION");
-		model.addAttribute("Registered", "");
-		setHeader(model);
-		return "RegisterTemplate";
-	}
-	
-	
-	@GetMapping("/login")
-	public String getLogin(Model model) {
-		model.addAttribute("site", "INICIAR SESION");
-		model.addAttribute("Registered", "");
-		setHeader(model);
-		if(this.loggedUser.isLoggedUser()) {
-			return "UserTemplate";
-		}
-		return "LoginTemplate";
-	}
-	
-
-	@GetMapping("/loggout")
-	public String LoggOut(HttpSession sesion, Model model) {
-		this.loggedUser.logOut();
-		return getProfile(model);
-	}
-	
-
-	@PostMapping("/login")
-	public String logearUsuario(@RequestParam String email, @RequestParam String password, HttpSession sesion,
-			Model model) {
-		UserEntity aux = urepository.findByEmail(email);
-		if (aux != null) {
-			if (aux.getPassword().equals(password)) {
-				this.loggedUser.setLoggedUser(aux);
-			} else {
-				model.addAttribute("Registered", "La contraseña va a ser que no es");
-			}
-		} else {
-			model.addAttribute("Registered", "Esa cuenta no existe");
-		}
-		return getProfile(model);
-	}
-*/
 
 	@PostMapping("/editPassword")
 	public String editPassword(@RequestParam String password, @RequestParam String password_repeated,
 			@RequestParam String new_password, HttpSession sesion) {
-		User aux = this.loggedUser.getLoggedUser();
+		User aux = this.userComponent.getLoggedUser();
 		if (aux.getPassword().equals(password) && password.equals(password_repeated)) {
 			aux.setPassword(new_password);
 		}
@@ -375,7 +211,7 @@ public class UserController {
 	@PostMapping("/editProfile")
 	public String editProfile(@RequestParam String name, @RequestParam String surname,
 			@RequestParam MultipartFile image, HttpSession sesion) throws IOException {
-		User aux = this.loggedUser.getLoggedUser();
+		User aux = this.userComponent.getLoggedUser();
 		Files.createDirectories(IMAGES_FOLDER);
 		Path imagePath = IMAGES_FOLDER.resolve("image" + aux.getEmail() + ".jpg");
 		image.transferTo(imagePath);
@@ -388,53 +224,4 @@ public class UserController {
 		urepository.save(aux);
 		return "EditProfileTemplate";
 	}
-
-	/*
-
-	@PostMapping("/register")
-	public String registrarUsuario(@RequestParam String name, @RequestParam String lastName, @RequestParam String email,
-			@RequestParam String password, HttpSession sesion, Model model) {
-		UserEntity aux = urepository.findByEmail(email);
-		if (aux != null) {
-			model.addAttribute("Registered", "Ya hay un usuario registrado con ese correo");
-		} else {
-			UserEntity user = new UserEntity(name, lastName, password, email);
-			urepository.save(user);
-			model.addAttribute("Registered", "Te has registrado correctamente :D");
-		}
-		return getLogin(model);
-	}
-	
-
-	@PostMapping("/createEvent")
-	public String registrarUsuario(@RequestParam String name, @RequestParam String description,
-			@RequestParam Integer capacity, @RequestParam String labels, @RequestParam String end, HttpSession sesion,
-			Model model) {
-		Event event;
-		if (this.editedEvent != null) {
-			event = this.editedEvent;
-			this.editedEvent = null;
-			for (String var : labels.split("/")) {
-				event.putLavel(var.toUpperCase());
-				if (!Event.allLabels.contains(var.toUpperCase())) {
-					Event.allLabels.add(var.toUpperCase());
-				}
-			}
-			event.setCapacity(capacity!=0 ? capacity : event.getCapacity());
-			event.setDescription(description.equals("") ? event.getDescription() : description);
-			event.setName(name.equals("") ? event.getName() : name);
-			event.setDate(end.equals("") ? event.getDate().toString() : end);
-		} else {
-			event = new Event(name, description, end, "", capacity);
-			for (String var : labels.split("/")) {
-				event.putLavel(var.toUpperCase());
-				if (!Event.allLabels.contains(var.toUpperCase())) {
-					Event.allLabels.add(var.toUpperCase());
-				}
-			}
-		}
-		eRepository.save(event);
-		return getAdmin(model);
-	}
-*/
 }
