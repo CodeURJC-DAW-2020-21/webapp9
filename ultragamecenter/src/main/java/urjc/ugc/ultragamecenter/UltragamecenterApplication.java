28090176425cdb18e84c1fdaba6c7af52c8cf4c3
Rod <roddesiqueira@gmail.com>
2021-04-18 23:37:42 +0200

    Resolution of conflicts

--- ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/UltragamecenterApplication.java
+++ ultragamecenter/src/main/java/urjc/ugc/ultragamecenter/UltragamecenterApplication.java
@@ -1,7 +1,15 @@
 package urjc.ugc.ultragamecenter;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class UltragamecenterApplication {
@@ -10,4 +18,13 @@ public class UltragamecenterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UltragamecenterApplication.class, args);
 	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+    return new Docket(DocumentationType.SWAGGER_2)
+		.select()
+		.paths(PathSelectors.ant("/api/event"))
+		.build()
+		;
+  }
 }
