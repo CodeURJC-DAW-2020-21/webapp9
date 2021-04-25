6a298bc24de9b75457363ec0df28c45cce7d6d27
Rod <roddesiqueira@gmail.com>
2021-04-25 19:35:04 +0200

    Added Login, header and graphs table components

--- ultragamecenter/src/main/resources/angular-ugc/src/app/app.module.ts
+++ ultragamecenter/src/main/resources/angular-ugc/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphsTableComponent } from './graphs-table/graphs-table.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphsTableComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ ultragamecenter/src/main/resources/angular-ugc/src/app/graphs-table/graphs-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphs-table',
+  templateUrl: './graphs-table.component.html',
+  styleUrls: ['./graphs-table.component.css']
+})
+export class GraphsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
