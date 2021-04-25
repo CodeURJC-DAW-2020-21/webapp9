74c8765e9e912d926f00eb1f0f796671b152b0d0
jesus <jesuselez99@gmail.com>
2021-04-25 19:18:22 +0200

    Some components created

--- ultragamecenter/src/main/resources/angular-ugc/src/app/app.module.ts
+++ ultragamecenter/src/main/resources/angular-ugc/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { AdminComponent } from './admin/admin.component';
+import { EditEventComponent } from './edit-event/edit-event.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent,
+    AdminComponent,
+    EditEventComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
