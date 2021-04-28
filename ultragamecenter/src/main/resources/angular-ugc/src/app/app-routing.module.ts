b3e676374f86b434adb7b519b718fd548a233818
Miguel <micenor88@gmail.com>
2021-04-28 17:54:05 +0200

    admin services + components v1

--- ultragamecenter/src/main/resources/angular-ugc/src/app/app-routing.module.ts
+++ ultragamecenter/src/main/resources/angular-ugc/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 
 import { EventComponent } from './components/event/event.component';
 
 const routes: Routes = [
-  {path: 'events', component: EventComponent}
+  {path: 'events', component: EventComponent},
+  {path: 'admin', component: AdminComponent}
 ];
 
 @NgModule({
--- ultragamecenter/src/main/resources/angular-ugc/src/app/components/admin/admin.component.ts
+++ ultragamecenter/src/main/resources/angular-ugc/src/app/components/admin/admin.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
+@Injectable({ providedIn: 'root'})
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  events: Event[] = []
+  constructor(private service: AdminService) { } 
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.service.getEvents().subscribe(events => this.events = events)
   }
-
 }
