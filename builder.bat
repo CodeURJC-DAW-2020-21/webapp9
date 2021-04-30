c8f9f6ca34796101eae1369f615d70f5f1cd0263
santo2927 <santo2927@gmail.com>
2021-04-30 14:07:42 +0200

    finished script

--- builder.bat
+++ builder.bat
@@ -1,11 +1,20 @@
 ::Nos movemos al Frontend
+SET mypath=%~dp0
+
+SET newFile=%mypath%ultragamecenter\src\main\resources\static\new\
+
+echo %newFile%
+
 cd ./Frontend
 cmd /C ng build --prod --base-href="/new/"
+cd ../
 
+DEL /Q %newFile%
+
+mkdir %newFile%
 ::Construimos la aplicacion de angular y lo movemos al proyecto spring
-cd ./dist/angular-ugc
-
-copy * "../../../ultragamecenter/src/main/resources/static/new/"
+cd ./Frontend/dist/angular-ugc
+copy * %newFile%
 
 ::Contruimos el dockerfile
 cd ../../../ultragamecenter
