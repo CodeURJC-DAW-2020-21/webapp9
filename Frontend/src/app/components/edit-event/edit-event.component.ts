5b559d9854a5a076065bf108c0323052cbe316dd
Miguel <micenor88@gmail.com>
2021-05-02 12:13:32 +0200

    more security options

--- Frontend/src/app/components/edit-event/edit-event.component.ts
+++ Frontend/src/app/components/edit-event/edit-event.component.ts
@@ -1,7 +1,9 @@
 import { NONE_TYPE } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Events } from 'src/app/models/event.model';
 import { EventService } from 'src/app/services/event.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-edit-event',
@@ -20,10 +22,13 @@ export class EditEventComponent implements OnInit {
   @Input()
   id: number = 0;
 
-  constructor(private eService: EventService) { }
+  constructor(private eService: EventService, private uService: UserService,  private router: Router) { }
 
   ngOnInit(): void {
-    this.eService.getSingleEvent(this.id).subscribe(event => this.event=event);
+    if(!this.uService.isAdmin()){
+      this.router.navigate([""]);
+    }else{
+      this.eService.getSingleEvent(this.id).subscribe(event => this.event=event);
+    }
   }
-
 }
